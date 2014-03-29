4647cdd9a2e321a03a11f4cff7007b335bcfa079
Shinya Tsuda <wish.upon.a.planet@gmail.com>
2014-03-29 14:08:30 +0900

    Added macro to annotate deprecation.

--- include/madoka/common.h
+++ include/madoka/common.h
@@ -11,4 +11,14 @@
   void operator=(const TypeName&)
 #endif  // DISALLOW_COPY_AND_ASSIGN
 
+#ifndef MADOKA_DEPRECATED
+  #if defined(_MSC_VER)
+    #define MADOKA_DEPRECATED __declspec(deprecated)
+  #elif defined(__GNUC__)
+    #define MADOKA_DEPRECATED __attribute__((deprecated))
+  #else
+    #define MADOKA_DEPRECATED
+  #endif
+#endif  // MADOKA_DEPRECATED
+
 #endif  // MADOKA_COMMON_H_
